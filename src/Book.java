efdd4c255a4f866a1f8e99dc25bb76f98c5e2165
ilmaamli <ilmaamalia684@gmail.com>
2023-05-19 22:26:20 +0700

    upload project UTS

--- src/Book.java
+++ src/Book.java
@@ -8,7 +8,17 @@
  * @author User
  */
 public class Book {
+    String title, author, year;
+    
     public static void main(String[] args){
-        
+        Book book = new Book();
+        book.get_book_info("Laskar Pelangi", "Andrea Hirata", "2005");
+    }
+    void get_book_info(String judul, String penulis, String tahun ){
+        title = judul;
+        author = penulis;
+        year = tahun;
+        System.out.println("Judul : ["+title+"]"+", Penulis :["+author+"]"+", Tahun Terbit :["+year+"]");
+
     }
 }
